afee1756cee26228422bf61a559c4b8c29ce57be
Will Roberts <will.roberts@rz.hu-berlin.de>
2016-10-28 15:03:52 +0200

    drive.py: some hacking

--- drive.py
+++ drive.py
@@ -17,18 +17,21 @@ Google Cloud Speech API sample application using the REST API for
 async batch processing.
 """
 
-from apiclient.http import MediaIoBaseDownload
+from apiclient.http import MediaIoBaseDownload, MediaIoBaseUpload
 from googleapiclient import discovery
 from oauth2client import client
 from oauth2client import tools
 from oauth2client.file import Storage
 import httplib2
 import json
+import os
+import subprocess
 
 # persistance
 def store_data(json_filename, data):
     with open(json_filename, 'w') as output_file:
-        output_file.write(json.dumps(data, ensure_ascii=False).encode('utf-8'))
+        json_data = json.dumps(data, sort_keys=True, indent=4, ensure_ascii=False)
+        output_file.write(json_data.encode('utf-8'))
 
 def load_data(json_filename):
     with open(json_filename, 'r') as input_file:
@@ -72,9 +75,10 @@ def main():
         speech_file: the name of the audio file.
     """
     service = get_drive_service()
-    folder_id = get_folder_id(service, 'Exams')
+    folder_name = 'Exams'
+    folder_id = get_folder_id(service, folder_name)
     if folder_id is None:
-        print 'ERROR: could not find Google Drive folder "Exams"'
+        print 'ERROR: could not find Google Drive folder "{}"'.format(folder_name)
         return
     results = service.files().list(
         pageSize=100,
@@ -86,7 +90,7 @@ def main():
 
 def download_file(drive_service, file_id, output_filename, verbose = False):
     request = drive_service.files().get_media(fileId = file_id)
-    with open(output_filename, 'w') as output_file:
+    with open(output_filename, 'wb') as output_file:
         downloader = MediaIoBaseDownload(output_file, request)
         done = False
         while done is False:
@@ -94,7 +98,61 @@ def download_file(drive_service, file_id, output_filename, verbose = False):
             if verbose:
                 print "Download %d%%." % int(status.progress() * 100)
 
-#download_file(service, results['files'][0]['id'], os.path.join('amr_files', results['files'][0]['name']), True)
+def run_command(args):
+    return subprocess.call(args)
+
+def local_amr_path(filename):
+    return os.path.join('amr_files', filename)
+
+def local_wav_path(filename):
+    return os.path.join('wav_files', filename.replace('.amr', '.wav'))
+
+#download_file(service, results['files'][0]['id'], local_amr_path(results['files'][0]['name']), True)
+FFMPEG = subprocess.check_output(['which', 'ffmpeg']).strip()
+#run_command([FFMPEG, '-i', local_amr_path(results['files'][0]['name']), local_wav_path(results['files'][0]['name'])])
+SOX = subprocess.check_output(['which', 'sox']).strip()
+# sox trim-silence.wav trim-silence-out.wav silence -l 1 0.1 0.1% -1 2.0 0.1%
+
+def get_cloud_storage_service():
+    http = get_service_acct_authorised_http()
+    return discovery.build('storage', 'v1', http=http)
+
+# https://cloud.google.com/storage/docs/json_api/v1/json-api-python-samples
+def cloud_upload_object(bucket, filename):
+    service = get_cloud_storage_service()
+
+    # This is the request body as specified:
+    # http://g.co/cloud/storage/docs/json_api/v1/objects/insert#request
+    body = {
+        'name': os.path.basename(filename),
+    }
+
+    # Now insert them into the specified bucket as a media insertion.
+    # http://g.co/dv/resources/api-libraries/documentation/storage/v1/python/latest/storage_v1.objects.html#insert
+    with open(filename, 'rb') as input_file:
+        req = service.objects().insert(
+            bucket=bucket, body=body,
+            # You can also just set media_body=filename, but for the sake of
+            # demonstration, pass in the more generic file handle, which could
+            # very well be a StringIO or similar.
+            media_body=MediaIoBaseUpload(input_file, 'application/octet-stream'))
+        resp = req.execute()
+        
+    return resp
+
+# https://cloud.google.com/storage/docs/json_api/v1/json-api-python-samples
+def cloud_delete_object(bucket, filename):
+    service = get_cloud_storage_service()
+
+    req = service.objects().delete(bucket=bucket, object=filename)
+    resp = req.execute()
+
+    return resp
+
+filename= 'amr_files/report3.amr'
+bucket = 'semantics-exam-marking.appspot.com'
+os.stat(filename).st_size == int(resp['size'])
+
 
 # [START run_application]
 if __name__ == '__main__':
