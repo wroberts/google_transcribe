4768d4034f7509d357ef061625fe1761579169f7
Will Roberts <will.roberts@rz.hu-berlin.de>
2016-10-29 15:28:56 +0200

    setup.py: entry_points

--- setup.py
+++ setup.py
@@ -108,11 +108,11 @@ setup(
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # pip to create the appropriate form of executable for the target platform.
-    #entry_points={
-    #    'console_scripts': [
-    #        'google_transcribe=sample:main',
-    #    ],
-    #},
+    entry_points={
+        'console_scripts': [
+            'google-transcribe=google_transcribe.transcribe:main',
+        ],
+    },
     test_suite='nose.collector',
     tests_require=['nose'],
 )
